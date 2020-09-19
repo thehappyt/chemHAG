cc041e43ec800cdca5cab88e4a283cbb4e1a56cf
Adam Thompson <thompson.adams+git@gmail.com>
2020-09-18 23:44:39 -0700

    Update object.js
    Fix several syntax issues.

--- objects.js
+++ objects.js
@@ -11,25 +11,27 @@ function GraduatedCylinder(args) {
   var txtH = args.fontSize = args.fontSize || 0.2*cylH*stpN/maxV;
   var base = args.base = (args.base==undefined?true:!!args.base);
   
+  scene.background=color.white
   var cyl = [];
-  cyl.push( extrusion(pos=vec(0,0.55*cylH,0), path=paths.circle(radius=cylR+0.5*thkL), shape=shapes.rectangle(width=thkL, height=1.1*cylH, roundness=thkL/4, pos=[-thkL/2,0]), color=color.white, opacity=0.05) )
-  for (let i=0; i<maxV+1; i+=stpL) {
+  cyl.push( extrusion({pos:vec(0,0.555*cylH,0), path:paths.circle({radius:cylR+0.5*thkL}), shape:shapes.rectangle({width:thkL, height:1.09*cylH, roundness:thkL/4, pos:[-thkL/2,0]}), color:color.white, opacity:0.05}) )
+  for (let i=2*stpL; i<maxV+1; i+=stpL) {
     let N=(i%stpN==0), an1=pi/8, an2=pi/4, an3=3*pi/16, n1=(1+Math.cos(an1))/2, n2=(1+Math.cos(an2))/2, hi=cylH*i/maxV, ax=vec(0,1,0);
     if (N) {
-      cyl.push( extrusion(pos=vec(cylR*n1+thkL,hi-thkL,0), path=paths.arc(radius=cylR, angle1=-an2, angle2=an2), shape=shapes.rectangle(width=thkL, height=thkL), color=color.black) )
-      cyl.push( text(text=i, align='center', height=txtH, color=color.black, pos=rotate(vec(cylR,hi,0), angle=an3, axis=ax), axis=rotate(vec(0,0,-1), angle=an3, axis=ax) ) )
+      cyl.push( extrusion({pos:vec(cylR*n2+thkL,hi-thkL,0), path:paths.arc({radius:cylR, angle1:-an2, angle2:an2}), shape:shapes.rectangle({width:thkL, height:thkL}), color:color.black}) )
+      cyl.push( text({text:i, align:'center', height:txtH, color:color.black, pos:vec(cylR,hi,0).rotate({angle:an3, axis:ax}), axis:vec(0,0,-1).rotate({angle:an3, axis:ax}) }) )
     } else {
-      cyl.push( extrusion(pos=vec(cylR*n2+thkL,hi-thkL,0), path=paths.arc(radius=cylR, angle1=-an1, angle2=an1), shape=shapes.rectangle(width=thkL, height=thkL), color=color.black) )
+      cyl.push( extrusion({pos:vec(cylR*n1+thkL,hi-thkL,0), path:paths.arc({radius:cylR, angle1:-an1, angle2:an1}), shape:shapes.rectangle({width:thkL, height:thkL}), color:color.black}) )
     }
   }
   if (base) {
-    cyl.append( extrusion(pos=vec(0,h/2,0),    color=color.yellow, path=paths.circle(radius=cylR-0.5*thkL), shape=shapes.rectangle(width=thkL,   height=h,    pos=[-0.5*thkL,0])) )
-    cyl.append( extrusion(pos=vec(0,h/2,0),    color=color.yellow, path=paths.circle(radius=cylR+1.5*thkL), shape=shapes.rectangle(width=thkL,   height=h,    pos=[-0.5*thkL,0])) )
-    cyl.append( extrusion(pos=vec(0,thkL/2,0), color=color.yellow, path=paths.circle(radius=2*cylR),        shape=shapes.rectangle(width=2*cylR, height=thkL, pos=[-cylR,0])) )
-    cyl.append( extrusion(pos=vec(0,h/2,0),    color=color.yellow, path=paths.circle(radius=cylR),          shape=shapes.arc(radius=cylR, angle1=pi, angle2=3*pi/2, thickness=0.05, pos=[cylR,0])) )
+    let h=cylH*stpL/maxV;
+    cyl.push( extrusion({ pos:vec(0,h/2,0),    color:color.yellow, path:paths.circle({ radius:cylR-0.5*thkL }), shape:shapes.rectangle({ width:thkL,   height:h,    pos:[-0.5*thkL,0] }) }) )
+    cyl.push( extrusion({ pos:vec(0,h/2,0),    color:color.yellow, path:paths.circle({ radius:cylR+1.5*thkL }), shape:shapes.rectangle({ width:thkL,   height:h,    pos:[-0.5*thkL,0] }) }) )
+    cyl.push( extrusion({ pos:vec(0,thkL/2,0), color:color.yellow, path:paths.circle({ radius:2*cylR }),        shape:shapes.rectangle({ width:2*cylR, height:thkL, pos:[-cylR,0] }) }) )
+    cyl.push( extrusion({ pos:vec(0,h/2,0),    color:color.yellow, path:paths.circle({ radius:cylR }),          shape:shapes.arc({ radius:cylR, angle1:pi, angle2:3*pi/2, thickness:0.05, pos:[cylR,0] }) }) )
   }
-  cyl = compound(cyl, origin=vec(0,0,0))
-  cyl.rotate( angle=-pi/2, axis=vec(0,1,0), origin=vec(0,0,0) )
+  cyl = compound(cyl, { origin:vec(0,0,0) })
+  cyl.rotate({ angle:-pi/2, axis:vec(0,1,0), origin:vec(0,0,0) })
   //cyl.pos = vec(-10,0,0)
   //maxH=0.9*cylH
   //cyl.fluid = cylinder(pos=vec(0,-cylH/2,0), axis=vec(0,0.4*maxH,0), radius=cylR, opacity=0.5, color=(color.blue+color.gray(1.0))/2)
